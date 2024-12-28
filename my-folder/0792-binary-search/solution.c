420984ba3160ae7627b37ca1f99c9b3ccee3ce65
Vishal Bharath <vishalbharathonly@gmail.com>
2024-12-28 04:01:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (55.24%)

---
+++ my-folder/0792-binary-search/solution.c
@@ -0,0 +1,18 @@
+int search(int* nums, int size, int target) {
+    int left=0;
+    int right=size-1;
+    while(left<=right)
+    {
+        int mid=(left+right)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){ 
+            left=mid+1;
+        }
+        else{ 
+            right=mid-1;
+        }
+    }
+    return -1;
+}
