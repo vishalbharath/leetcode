3001f355c48136e6b9eb48c4b020ed05dafd2d8a
Vishal Bharath <vishalbharathonly@gmail.com>
2025-01-03 13:36:09 +0000

    [LeetCode Sync] Runtime - 3 ms (1.27%), Memory - 8.3 MB (21.48%)

---
+++ my-folder/0504-base-7/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num == 0 ) return "0";
+        string result;
+        bool isNegative = (num<0);
+        num = abs(num);
+        while(num >0){
+            result = to_string(num % 7) + result;
+            num /= 7;
+        }
+        return (isNegative)? '-' + result : result;
+        
+    }
+};
