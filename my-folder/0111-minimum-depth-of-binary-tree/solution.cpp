e5119e02560aada236b2c45a1564bddf97aac0d7
Vishal Bharath <vishalbharathonly@gmail.com>
2025-01-04 09:51:00 +0000

    [LeetCode Sync] Runtime - 9 ms (7.20%), Memory - 146.6 MB (79.82%)

---
+++ my-folder/0111-minimum-depth-of-binary-tree/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        
+        if(root==NULL) return 0;
+
+        if(root->left==NULL){
+          return 1+minDepth(root->right);
+        }
+         if(root->right==NULL){
+          return 1+minDepth(root->left);
+        }
+
+        return 1+min(minDepth(root->right), minDepth(root->left));
+    }
+};
