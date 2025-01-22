5dac77c47b075384d5cc87503af5d0155a3a6251
Vishal Bharath <vishalbharathonly@gmail.com>
2025-01-22 08:23:41 +0000

    [LeetCode Sync] Runtime - 2 ms (78.22%), Memory - 43.8 MB (88.22%)

--- my-folder/0120-triangle/solution.java
+++ my-folder/0120-triangle/solution.java
@@ -1,13 +1,15 @@
-
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n=triangle.size();
-        Integer[][] dp=new Integer[n][n];
-        return findMin(triangle,0,0,n,dp);
-    }
-    public static int findMin(List<List<Integer>> tri,int i,int j,int n,Integer[][] dp){
-        if(i==n-1) return tri.get(i).get(j);
-        if(dp[i][j]!=null) return dp[i][j];
-        return dp[i][j]=tri.get(i).get(j)+Math.min(findMin(tri,i+1,j,n,dp),findMin(tri,i+1,j+1,n,dp));
+        int row = triangle.size();
+        int[] memo = new int[row];
+        for (int i = 0; i < row; i++) {
+            memo[i] = triangle.get(row - 1).get(i);
+        }
+        for (int r = row-2; r >= 0; r--) {
+            for (int c = 0; c <= r; c++) {
+                memo[c] = Math.min(memo[c], memo[c+1]) + triangle.get(r).get(c);
+            }
+        }
+        return memo[0];        
     }
 }
