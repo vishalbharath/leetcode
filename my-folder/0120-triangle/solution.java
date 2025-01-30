3e33eb31ef10693c834d1d94fa035d16c1dbde94
Vishal Bharath <vishalbharathonly@gmail.com>
2025-01-30 13:04:08 +0000

    [LeetCode Sync] Runtime - 3 ms (66.26%), Memory - 43.7 MB (94.14%)

--- my-folder/0120-triangle/solution.java
+++ my-folder/0120-triangle/solution.java
@@ -1,15 +1,22 @@
+import java.util.List;
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int row = triangle.size();
-        int[] memo = new int[row];
-        for (int i = 0; i < row; i++) {
-            memo[i] = triangle.get(row - 1).get(i);
+        int n = triangle.size();
+        int[] dp = new int[n];
+
+        // Initialize the dp array with the last row of the triangle
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
         }
-        for (int r = row-2; r >= 0; r--) {
-            for (int c = 0; c <= r; c++) {
-                memo[c] = Math.min(memo[c], memo[c+1]) + triangle.get(r).get(c);
+
+        // Build the dp array from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
-        return memo[0];        
+
+        return dp[0];
     }
 }
