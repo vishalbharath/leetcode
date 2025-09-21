574aabd0d31bdd2e7f4acbcbc849cdec5114b882
Vishal Bharath <vishalbharathonly@gmail.com>
2025-09-21 20:40:23 +0530

    Time: 1389 ms (43.74%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+SELECT Visits.customer_id, COUNT(Visits.visit_id) as count_no_trans FROM Visits
+LEFT JOIN Transactions
+ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.transaction_id IS NULL
+GROUP BY Visits.customer_id;
+
+
