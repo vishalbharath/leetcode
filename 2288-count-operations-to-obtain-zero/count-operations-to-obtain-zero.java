2c6880aa9b4d4b0520e3bf50dc71eb803d7d621f
Vishal Bharath <vishalbharathonly@gmail.com>
2025-11-09 14:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (21.46%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOperations(int n1, int n2) {
+        int c = 0;
+        while (n1 > 0 && n2 > 0) {
+            c += n1 / n2;
+            n1 %= n2;
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+        return c;
+    }
+}
