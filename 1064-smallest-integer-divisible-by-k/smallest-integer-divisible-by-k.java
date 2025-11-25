1e5c545eac498a5b543c75937f560a53ef2e68e4
Vishal Bharath <vishalbharathonly@gmail.com>
2025-11-25 18:31:22 +0530

    Time: 2 ms (97.52%) | Memory: 41.5 MB (81.68%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if ( (k&1)==0|| k%5==0) return -1; 
+        if (k==1) return 1; 
+        int r=1;        
+        for(int len0=2; ; len0++){
+            r=(10*r+1)%k;
+            if (r==0) return len0;
+        }
+    }
+}
