e1818a622c9c8f38f1d4ad713b40df73946ae3f3
Vishal Bharath <vishalbharathonly@gmail.com>
2025-12-08 18:43:58 +0530

    Time: 1 ms (98.12%) | Memory: 42 MB (59.50%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -0,0 +1,33 @@
+class Solution {
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public int countTriples(int n) {
+        int cnt = 0;
+        
+        // Iterate m starting from 2
+        for (int m = 2; m * m < n; m++) {
+            // Iterate k from 1 to m
+            for (int k = 1; k < m; k++) {
+                // Check for primitive triple conditions:
+                // 1. gcd(m, k) == 1 (coprime)
+                // 2. One is even, one is odd -> (m - k) is odd
+                if (gcd(m, k) == 1 && (m - k) % 2 == 1) {
+                    int c_primitive = m * m + k * k;
+                    
+                    if (c_primitive > n) {
+                        break;
+                    }
+                    cnt += 2 * (n / c_primitive);
+                }
+            }
+        }
+        return cnt;
+    }
+}
