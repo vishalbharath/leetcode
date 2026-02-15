7242fcb2293c971e0340ecbcaa688ceaa1457efb
Vishal Bharath <vishalbharathonly@gmail.com>
2026-02-15 12:17:54 +0530

    Time: 2584 ms (5.00%) | Memory: 47.1 MB (19.03%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static boolean isPalindrome(String s){
+        int left = 0, right = s.length() - 1;
+
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public String longestPalindrome(String s) {
+        String longest = "";
+
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+
+                String sub = s.substring(i, j+1);
+
+                if(isPalindrome(sub) && sub.length() > longest.length()){
+                    longest = sub;
+                }
+            }
+        }
+        System.out.println("Longest Palindromic Substring = " + longest);
+        return longest;
+}
+}
